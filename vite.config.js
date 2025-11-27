30be15deefb2e6d85b27fe95da4d52775628456d
siriusbrak7 <bbrak644@gmail.com>
2025-11-27 23:05:31 +0000

    force port 3000 for Render

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    port: 3000,
+    host: '0.0.0.0',
     allowedHosts: ['bio-lens.onrender.com', '.onrender.com']
   }
 })
