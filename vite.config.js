ec61b528fa807315dbdf46914b596c8a15b530c5
siriusbrak7 <bbrak644@gmail.com>
2025-11-27 22:57:36 +0000

    fix vite config for render

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["bio-lens.onrender.com", ".onrender.com"]
+    host: true,
+    allowedHosts: ['bio-lens.onrender.com', '.onrender.com']
   }
 })
